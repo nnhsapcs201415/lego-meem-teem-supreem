e440af88adcf8396b526befc3d22d0d736c20640
caprexy <CCSiu@student.naperville203.org>
2017-05-17 14:39:41 -0500

    idk wtf is goin on

--- RobotCode/Challenge.java
+++ RobotCode/Challenge.java
@@ -1,28 +1,41 @@
 import lejos.nxt.*;
 import lejos.robotics.objectdetection.*;
 import lejos.robotics.navigation.DifferentialPilot;
+import lejos.nxt.LCD;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
+import lejos.nxt.Motor;
+import lejos.nxt.Button;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.nxt.SensorPortListener;
 
 public class Challenge implements FeatureListener, SensorPortListener {
-
-    public static DifferentialPilot pilot;
-    public static int MAX_DETECT = 80;
+    private static DifferentialPilot pilot;
     
-    public static void main(String[] args) {
-        Challenge C = new Challenge();
-        UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
-        LightSensor light = new LightSensor(SensorPort.S2);
-        RangeFeatureDetector fd = new RangeFeatureDetector(us, MAX_DETECT, 500);
-        fd.addListener(C);
-        C.pilot = new DifferentialPilot(5.315f, 5.355f, 11.4f, Motor.A, Motor.C, false);
-        pilot.travel(500);
-        Button.ENTER.waitForPressAndRelease();
-    }
-    
-    public void featureDetected(Feature feature, FeatureDetector detector) {
-        pilot.stop();
-    }
-    
-    public void stateChanged(SensorPort aSource, int aOldValue, int aNewValue) {
+    public Challenge(String[] args) 
+    {
+        Challenge c = new Challenge();
         
+        c.pilot = new DifferentialPilot(5.315f, 5.355f, 11.4f, 
+            Motor.A, Motor.C, false);
+            
+        circlePattern();
+    }
+
+    public static void circlePattern()
+    {
+        while(true)
+        {
+            
+        }
+    } 
+
+    public void featureDetected(Feature feature, FeatureDetector detector)
+    {
+    }
+
+    public void stateChanged(SensorPort aSource, int aOldValue, int aNewValue) 
+    {
+
     }
 }
