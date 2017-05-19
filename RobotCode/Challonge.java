7b45c07c64d9a9ab01d21247245a39ed6148d953
Richard Li <diamondspell13@gmail.com>
2017-05-19 14:14:56 -0500

    aaaaaaaaaaa

--- RobotCode/Challonge.java
+++ RobotCode/Challonge.java
@@ -12,45 +12,45 @@ import lejos.nxt.SensorPortListener;
 public class Challonge implements FeatureListener, SensorPortListener {
 
     public DifferentialPilot pilot;
-    public int MAX_DETECT = 80;
-    
+    public int MAX_DETECT = 100;
+
     public Challonge() {
         UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
+        RangeFeatureDetector fd = new RangeFeatureDetector(us, MAX_DETECT, 20);
         SensorPort.S2.addSensorPortListener(this);
-        RangeFeatureDetector fd = new RangeFeatureDetector(us, MAX_DETECT, 500);
         fd.addListener(this);
         this.pilot = new DifferentialPilot(5.315f, 5.355f, 11.4f, Motor.A, Motor.C, false);
-
     }
 
-    public static void circlePattern()
+    public void circlePattern()
     {
         while(true)
         {
-            
+            pilot.steer(10);
         }
     } 
 
     public void featureDetected(Feature feature, FeatureDetector detector)
     {
+        LCD.drawString("detected",3,4);
+        pilot.forward();
+        // use a world wide var to stop when 
     }
-    
+
     public void stateChanged(SensorPort aSource, int aOldValue, int aNewValue) {
         LCD.drawString(" "+aNewValue,3,4);
         if (aNewValue >= 860) {
-            pilot.quickStop();
-            LCD.drawString(" "+aNewValue,2,4);
-            pilot.rotate(180);
-            pilot.forward();
+            pilot.travel(-10);
+            pilot.setRotateSpeed(100);
+            pilot.rotateLeft();
         }
     }
-    
+
     public void go() {
         pilot.forward();
     }
-    
+
     public void go(double d) {
         pilot.travel(d, true);
-        
     }
 }
