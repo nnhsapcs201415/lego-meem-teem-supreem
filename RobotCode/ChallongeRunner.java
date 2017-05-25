c86b306b39c6cdc1ba054e99ad3cacd324466a4d
Richard Li <diamondspell13@gmail.com>
2017-05-25 08:05:48 -0500

    smol

--- RobotCode/ChallongeRunner.java
+++ RobotCode/ChallongeRunner.java
@@ -5,8 +5,6 @@ import lejos.robotics.navigation.DifferentialPilot;
 public class ChallongeRunner {
     public static void main(String[] args) {
         Challonge c = new Challonge();
-        c.go();
-        Button.ENTER.waitForPressAndRelease();
         for (;;) {
             if (Button.readButtons() > 0) break;
         }
